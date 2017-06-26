4e9a61f6bea7cca6492a9a846f027a7cf3fecab5
Derek Chan <derekchan7@gmail.com>
2017-06-25 23:59:30 -0400

    Fix mongoose connection

--- project/app.js
+++ project/app.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/hs-theorycraftr';
 
 if (process.env.MLAB_USERNAME_WEBDEV) {
@@ -7,10 +8,8 @@ if (process.env.MLAB_USERNAME_WEBDEV) {
   connectionString += '@ds139362.mlab.com:39362/heroku_nzzfqv1s';
 }
 
-var mongoose = require('mongoose');
-
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://localhost/hs-theorycraftr');
+mongoose.connect('connectionString');
 
 require('./services/user.service.server.js');
 require('./services/deck.service.server.js');
