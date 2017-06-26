ca97a7d955dc4c209ac8b008574d850f64214ed4
Derek Chan <derekchan7@gmail.com>
2017-06-25 21:28:05 -0400

    Setup Heroku

--- project/app.js
+++ project/app.js
@@ -1,3 +1,12 @@
+var connectionString = 'mongodb://127.0.0.1:27017/hs-theorycraftr';
+
+if (process.env.MLAB_USERNAME_WEBDEV) {
+  var username = process.env.MLAB_USERNAME_HS-THEORYCRAFTR;
+  var password = process.env.MLAB_PASSWORD_HS-THEORYCRAFTR;
+  connectionString = 'mongodb://' + username + ':' + password;
+  connectionString += '@ds139362.mlab.com:39362/heroku_nzzfqv1s';
+}
+
 var mongoose = require('mongoose');
 
 mongoose.Promise = require('q').Promise;
