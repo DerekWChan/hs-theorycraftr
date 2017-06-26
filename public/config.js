433d1d6f6a84825a253cbe806ce518ed22955721
Derek Chan <derekchan7@gmail.com>
2017-06-26 06:16:47 -0400

    Implement back button functionality

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@
         controller: "adminController",
         controllerAs: "model",
         resolve: {
-          currentUser: checkLoggedIn//checkAdmin
+          currentUser: checkAdmin//checkCurrentUser
         }
       })
       .when("/admin/users", {
@@ -26,7 +26,7 @@
         controller: "adminUsersController",
         controllerAs: "model",
         resolve: {
-          currentUser: checkLoggedIn//checkAdmin
+          currentUser: checkAdmin//checkCurrentUser
         }
       })
       .when("/login", {
